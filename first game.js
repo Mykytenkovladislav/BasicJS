4adbf907d5069c57a8db9a4d4fe55e192617c5a9
mykytenkovladislav <mykytenkovladislav@gmail.com>
2021-05-05 23:18:14 +0300

    First commit

--- first game.js
+++ first game.js
@@ -1,1 +1,6 @@
-let promptDialog = prompt('Enter your name:')
+let userName = prompt('Enter your name:');
+if (userName != null) {
+    console.log('Hi, ' + userName);
+}else {
+    console.log('Ok, noname!')
+}
