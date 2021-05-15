04e315fbe6495ac36f85812c73f62bc2b813f899
mykytenkovladislav <mykytenkovladislav@gmail.com>
2021-05-15 18:43:24 +0300

    small tasks from book

--- first game.js
+++ first game.js
@@ -1,32 +1,40 @@
-let wordsArray = ["Яблоко", "Банан", "Молоко", "Кастрюля"]
+let wordsArray = ["яблоко", "банан", "молоко", "кастрюля"]
+let answerArray = [];
+let attempts = 5;
+let wasGuessed = false;
 // choose random word
 let keyWord = wordsArray[Math.floor(Math.random() * wordsArray.length)];
-console.log("Key word: " + keyWord);
-let answerArray = [];
+alert("Key word: " + keyWord);
 // create result array
 for (let i = 0; i < keyWord.length; i++)
     answerArray[i] = "_";
 let remainingLetters = keyWord.length;
 // game cycle
-while (remainingLetters > 0) {
+while (remainingLetters > 0 && attempts !==0 ) {
+    wasGuessed = false
     alert('Your current result: ' + answerArray.join(" ")); // Result print
 
-    var letter = prompt("Pls, enter 1 letter(ru):"); // user input
+    const letter = prompt("Pls, enter 1 letter(ru):"); // user input
     if (letter === null) {
         break;
     } else if (letter.length !== 1) { // if more than 1 characters were entered
         alert("Pls, enter just 1 letter");
     } else { // status update
         for (let j = 0; j < keyWord.length; j++) {
-            if (keyWord[j] === letter) { // guess is correct
+            if (keyWord[j] === letter.toLowerCase()) { // guess is correct
                 if (answerArray[j] !== letter) {
                     answerArray[j] = letter
                     remainingLetters--;
+                    wasGuessed = true
                 } else {
                     alert("You already entered this letter. Pls, choose another letter.")
                     break;
                 }
             }
+        }
+        if (wasGuessed === false) {
+            attempts--;
+            alert("You didn't guess, so " + attempts + " attempts left")
         }
     }
 }
