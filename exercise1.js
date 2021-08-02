49efa78e4662af3f1d9a0e61e5d53da06b617547
vmykytenko <mykytenkovladislav@gmail.com>
2021-08-02 15:13:40 +0300

    Task 4.2 done

--- exercise1.js
+++ exercise1.js
@@ -1,21 +1,39 @@
 // Task 1
-function calcRectangleArea() {
-    let width = parseInt(prompt('Width is '));
-    let height = parseInt(prompt('Height is '));
-
-    if (isNaN(width) || isNaN(height)){
-        throw new TypeError('Not a number');
-    }
-    return width*height;
-}
-
-try {
-    console.log(calcRectangleArea());
-} catch (error) {
-    console.log(error.name);
-    console.log(error.message);
-    console.log(error.stack);
-}
+// function calcRectangleArea() {
+//     let width = parseInt(prompt('Width is '));
+//     let height = parseInt(prompt('Height is '));
+//
+//     if (isNaN(width) || isNaN(height)) {
+//         throw new TypeError('Not a number');
+//     }
+//     return width * height;
+// }
+//
+// try {
+//     console.log(calcRectangleArea());
+// } catch (error) {
+//     console.log(error.name);
+//     console.log(error.message);
+//     console.log(error.stack);
+// }
 
 // Task 2
 
+function checkAge(age) {
+    if (age === '') {
+        throw Error('Empty string')
+    } else if (isNaN(age)) {
+        throw new TypeError('Not a number');
+    } else if (parseInt(age) < 14) {
+        throw new RangeError('Your age is less than 14')
+    }
+    return true
+}
+
+try {
+    if (checkAge('')) {
+        alert('Access granted')
+    }
+} catch (error) {
+    alert(error.name + '\n' + error.message)
+}
