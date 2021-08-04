01964ce02dd69495dd42feba077920049884c11b
vmykytenko <mykytenkovladislav@gmail.com>
2021-08-04 16:08:23 +0300

    Task 6.2 questions

--- exercise1.js
+++ exercise1.js
@@ -2,3 +2,7 @@
 // Variant 1
 // document.getElementById('test').innerHTML = 'Last'
 // document.body.children[1].innerText = 'Last'
+
+// Task 2
+document.getElementsByClassName("image")[0].src="cat.jpg";
+
